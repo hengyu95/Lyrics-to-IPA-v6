8411d8289440a24c1292c1fea9b371843f0cec12
Heng <limhengyu95@gmail.com>
2026-05-15 15:54:39 +0800

    Delete

--- test.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QTextEdit
-
-app = QApplication([])
-editor = QTextEdit()
-editor.setOpenExternalLinks(False)  # Test setting without subclassing
-editor.show()
-app.exec_()
